3585a108136964414734665657a6afc574e830df
yangyu <1911466766@qq.com>
2024-03-02 17:30:16 +0800

    first commit

---
+++ model/model_param_init.m
@@ -0,0 +1,15 @@
+CTRL_CONST.dt = 0.005;
+
+CTRL_PARAM.UAV_ID   = 1;
+CTRL_PARAM.ADJ_MARTIX = [1 1 1;1 1 1;1 1 1];                % Adjacency matrix --> A
+CTRL_PARAM.REL_X_MATRIX = [0 -40 -20;40 0 20;20 -20 0];     % Postion X Relation matrix --> Rx
+CTRL_PARAM.REL_Y_MATRIX = [0 20 40;-20 0 20;-40 -20 0];     % Postion Y Relation matrix --> Ry
+CTRL_PARAM.REL_Z_MATRIX = [0 0 0;0 0 0;0 0 0];              % Postion Z Relation matrix --> Rz
+CTRL_PARAM.NUM_UAV  = size(CTRL_PARAM.ADJ_MARTIX, 1);       % Num of uav at now.
+
+
+
+CTRL_PARAM.W_MIN = single(1.0);     % deg / s 
+CTRL_PARAM.AIRSPD_MIN   = single(10.0); % Minimum Airspeed (CAS)
+CTRL_PARAM.AIRSPD_MAX   = single(20.0); % Maximum Airspeed (CAS)
+CTRL_PARAM.AIRSPD_TRIM  = single(15.0); % Trim (Cruise) Airspeed
