c387fea418c59802df266147c93aa5b047556897
yangyu <1911466766@qq.com>
2024-03-19 20:48:41 +0800

    2024-03-19 20:48:41 autopush

--- scipt/compareModelOut.m
+++ scipt/compareModelOut.m
@@ -6,7 +6,8 @@ ins = get(out, 'INS');
 plot(ins.x_R.Data(:, 1), ins.y_R.Data(:, 1), ...
      ins.x_R.Data(:, 2), ins.y_R.Data(:, 2), ...
      ins.x_R.Data(:, 3), ins.y_R.Data(:, 3));
-
+title('XoY');
+axis equal;
 % PN = get(out.logsout, 'PN').Values.Data;
 % PE = get(out.logsout, 'PE').Values.Data;
 % plot(PN,PE);
